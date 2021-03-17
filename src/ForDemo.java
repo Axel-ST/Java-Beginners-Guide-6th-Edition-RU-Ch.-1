867d0d920e2b077ee5d263863665650778e1c580
Axel_ST <kozlov.axe@gmail.com>
2021-03-17 13:43:20 +0700

    Edit ForDemo class
    Java: А Beginners Guide, 6th Edition (RU) Ch. 1 p. 49

--- src/ForDemo.java
+++ src/ForDemo.java
@@ -1,6 +1,16 @@
 /*
-* Created by Axel_ST on 17.03.2021
-*/
+ * Created by Axel_ST on 17.03.2021
+ *
+ * Java: А Beginners Guide, 6th Edition (RU) Ch. 1 p. 49
+ */
 
-package PACKAGE_NAME;public class ForDemo {
+public class ForDemo {
+   public static void main(String[] args) {
+      int count;
+      
+      for (count = 0; count < 5; count++) {
+         System.out.println("Значение счетчика: " + count);
+      }
+      System.out.println("The end!");
+   }
 }
