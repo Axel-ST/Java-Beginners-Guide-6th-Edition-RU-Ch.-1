e97e9acec2b87606fe1c56274d0998f23fdbc341
Axel_ST <kozlov.axe@gmail.com>
2021-03-17 10:31:02 +0700

    Edit Example3 class
    Java: А Beginners Guide, 6th Edition (RU) Ch. 1 p. 44

--- src/Example3.java
+++ src/Example3.java
@@ -1,6 +1,23 @@
 /*
-* Created by Axel_ST on 17.03.2021
-*/
+ * Created by Axel_ST on 17.03.2021
+ *
+ * Java: А Beginners Guide, 6th Edition (RU) Ch. 1 p. 44
+ */
 
-package PACKAGE_NAME;public class Example3 {
+public class Example3 {
+   public static void main(String[] args) {
+      int var;
+      double x;
+      var = 10; x = 10.0;
+   
+      System.out.println("Начальное значение переменной var = " + var);
+      System.out.println("Начальное значение переменной x = " + x);
+      System.out.println();
+      
+      var /= 4;
+      x /= 4;
+   
+      System.out.println("Значение переменной var после деления на 4 = " + var);
+      System.out.println("Значение переменной x после деления на 4 = " + x);
+   }
 }
