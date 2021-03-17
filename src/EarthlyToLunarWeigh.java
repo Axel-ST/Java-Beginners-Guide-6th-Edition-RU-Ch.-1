00dc20e37ed30575ff40da47d4cd10e78aa1325f
Axel_ST <kozlov.axe@gmail.com>
2021-03-17 15:57:39 +0700

    Edit EarthlyToLunarWeigh class
    Java: А Beginners Guide, 6th Edition (RU) Ch. 1 ex. 9

--- src/EarthlyToLunarWeigh.java
+++ src/EarthlyToLunarWeigh.java
@@ -1,6 +1,18 @@
 /*
-* Created by Axel_ST on 17.03.2021
-*/
+ * Created by Axel_ST on 17.03.2021
+ *
+ * Java: А Beginners Guide, 6th Edition (RU) Ch. 1 ex. 9
+ */
 
-package PACKAGE_NAME;public class EarthlyToLunarWeigh {
+public class EarthlyToLunarWeigh {
+   public static void main(String[] args) {
+      double earthlyWeight, lunarWeight;
+      earthlyWeight = 66.5;
+      
+      lunarWeight = earthlyWeight * 0.17;
+   
+      System.out.println("Земной вес - " + earthlyWeight + "кг,");
+      System.out.println("а лунный - " + lunarWeight + "кг;");
+      System.out.println("но это не точно");
+   }
 }
