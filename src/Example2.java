80043fa1fcb839f39bae3997e75972198f3d831e
Axel_ST <kozlov.axe@gmail.com>
2021-03-17 10:13:48 +0700

    Edite Example2 class
    Java: А Beginners Guide, 6th Edition (RU) Ch. 1 p. 42

--- src/Example2.java
+++ src/Example2.java
@@ -1,6 +1,17 @@
 /*
-* Created by Axel_ST on 17.03.2021
-*/
+ * Created by Axel_ST on 17.03.2021
+ *
+ * Java: А Beginners Guide, 6th Edition (RU) Ch. 1 p. 42
+ */
 
-package PACKAGE_NAME;public class Example2 {
+public class Example2 {
+   public static void main(String[] args) {
+      int var1, var2;
+      var1 = 1024;
+      System.out.println("Переменная var1 содержит число " + var1);
+      
+      var2 = var1 / 2;
+      System.out.print("Переменная var2 содержит var1 / 2 = ");
+      System.out.println(var2);
+   }
 }
