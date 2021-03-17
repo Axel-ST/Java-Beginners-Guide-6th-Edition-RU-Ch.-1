7822f1ec4215f4afa3c2a612bbd234e1f982b067
Axel_ST <kozlov.axe@gmail.com>
2021-03-17 14:52:34 +0700

    Edit BlockDemo class
    Java: А Beginners Guide, 6th Edition (RU) Ch. 1 p. 50

--- src/BlockDemo.java
+++ src/BlockDemo.java
@@ -1,6 +1,19 @@
 /*
-* Created by Axel_ST on 17.03.2021
-*/
+ * Created by Axel_ST on 17.03.2021
+ *
+ * Java: А Beginners Guide, 6th Edition (RU) Ch. 1 p. 50
+ */
 
-package PACKAGE_NAME;public class BlockDemo {
+public class BlockDemo {
+   public static void main(String[] args) {
+      double i, j, k;
+      i = 5;
+      j = 10;
+      
+      if (i != 0){
+         System.out.println("i не равно 0.");
+         k = j/i;
+         System.out.println("j / i равно: " + k + ".");
+      }
+   }
 }
