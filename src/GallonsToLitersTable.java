b624c442d35b395c9de6aa9f01865ac906d15ae3
Axel_ST <kozlov.axe@gmail.com>
2021-03-17 15:44:13 +0700

    Edit GallonsToLitersTable class
    Java: А Beginners Guide, 6th Edition (RU) Ch. 1 p. 52-53 (ex. 1.2)

--- src/GallonsToLitersTable.java
+++ src/GallonsToLitersTable.java
@@ -1,6 +1,24 @@
 /*
-* Created by Axel_ST on 17.03.2021
-*/
+ * Created by Axel_ST on 17.03.2021
+ *
+ * Java: А Beginners Guide, 6th Edition (RU) Ch. 1 p. 52-53 (ex. 1.2)
+ */
 
-package PACKAGE_NAME;public class GallonsToLitersTable {
+public class GallonsToLitersTable {
+   public static void main(String[] args) {
+      double gallons, liters;
+      int counter;
+      
+      counter = 0;
+      for (gallons = 1; gallons <= 100; gallons++) {
+         liters = gallons * 3.7854;
+         System.out.println(gallons + " галлонам соответствует " + liters + " литров.");
+         counter++;
+         
+         if (counter == 10) {
+            counter = 0;
+            System.out.println();
+         }
+      }
+   }
 }
