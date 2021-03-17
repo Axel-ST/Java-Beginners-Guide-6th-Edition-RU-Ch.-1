b711616c07de751e305dd44eed95771b1f8d63b3
Axel_ST <kozlov.axe@gmail.com>
2021-03-17 11:15:46 +0700

    Edit GallonsToLiters class
    Java: А Beginners Guide, 6th Edition (RU) Ch. 1 p. 46 (ex 1.1)

--- src/GallonsToLiters.java
+++ src/GallonsToLiters.java
@@ -1,6 +1,16 @@
 /*
-* Created by Axel_ST on 17.03.2021
-*/
+ * Created by Axel_ST on 17.03.2021
+ *
+ * Java: А Beginners Guide, 6th Edition (RU) Ch. 1 p. 46 (ex 1.1)
+ */
 
-package PACKAGE_NAME;public class GallonsToLiters {
+public class GallonsToLiters {
+   public static void main(String[] args) {
+      double gallons, liters;
+      
+      gallons = 10;
+      liters = gallons * 3.7854;
+   
+      System.out.println(gallons + " галлонам соответствует " + liters + " литров.");
+   }
 }
