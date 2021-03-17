683f3413e1c40d8ada3c2e373277ad80d05fd1d4
Axel_ST <kozlov.axe@gmail.com>
2021-03-17 16:02:00 +0700

    Edit InchesToMetersTable class
    Java: А Beginners Guide, 6th Edition (RU) Ch. 1 ex. 10

--- src/InchesToMetersTable.java
+++ src/InchesToMetersTable.java
@@ -1,6 +1,23 @@
 /*
-* Created by Axel_ST on 17.03.2021
-*/
+ * Created by Axel_ST on 17.03.2021
+ *
+ * Java: А Beginners Guide, 6th Edition (RU) Ch. 1 ex. 10
+ */
 
-package PACKAGE_NAME;public class InchesToMetersTable {
+public class InchesToMetersTable {
+   public static void main(String[] args) {
+      double inches, meters;
+      int counter = 0;
+      
+      for (inches = 1; inches <= 144; inches++) {
+         meters = inches / 39.37;
+         System.out.println(inches + " дюймам соответствует " + meters + " метров.");
+         counter++;
+         
+         if (counter == 12) {
+            System.out.println();
+            counter++;
+         }
+      }
+   }
 }
